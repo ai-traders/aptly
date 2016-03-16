66eb7259baba1e75ab451da99d8cbda0d43d96ba
Ewa Czechowska <ewa@ai-traders.com>
2016-03-16 20:23:38 +0100

    respect options when using aptly_mirror resource
    Example of options: '-force-components'

--- resources/mirror.rb
+++ resources/mirror.rb
@@ -34,3 +34,4 @@ attribute :keyid, :kind_of => String, :default => nil
 attribute :keyserver, :kind_of => String, :default => nil
 attribute :keyfile, :kind_of => String, :default => nil
 attribute :filter, :kind_of => String, :default => nil
+attribute :options, :kind_of => String, :default => nil
